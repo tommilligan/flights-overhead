8d0d5beb448b1a0c137194d9abb92a49a2f8e304
Tom Milligan <tommilligan@users.noreply.github.com>
2018-07-29 17:38:34 +0100

    Working postcode api call

---
+++ README.md
@@ -0,0 +1,21 @@
+# flights-overhead
+
+## Outline
+
+```
+postcode
+   +
+   | postcodes.io/postcodes
+   v
+lat/long
+   +
+   | https://stackoverflow.com/a/1253545/2432585
+   v
+lat/long bbox
+   +
+   | https://opensky-network.org/api/all
+   v
+list of flights +-+-> log
+                  |
+                  +-> ascii map
+```
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,34 @@
 extern crate clap;
+extern crate reqwest;
 use clap::{Arg, App};
+
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct PostcodeApiResponse<T> {
+    status: u64,
+    result: T
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct PostcodeLocation {
+    country: String,
+    latitude: f64,
+    longitude: f64,
+    postcode: String,
+    region: String
+}
+
+/// Get location metadata from a postcode
+fn get_postcode_location(postcode: &str) -> PostcodeLocation {
+    let postcode_url = format!("https://api.postcodes.io/postcodes/{}", postcode);
+    let res: PostcodeApiResponse<PostcodeLocation> = reqwest::get(&postcode_url).unwrap().json().unwrap();
+    println!("response: {:?}", res);
+    res.result
+}
 
 /// Returns the clap command line parser
 fn main_parser<'a>() -> clap::App<'a, 'a> {
@@ -16,7 +45,9 @@ fn main() {
 
     // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
     // required we could have used an 'if let' to conditionally get the value)
-    println!("Using postcode: {}", matches.value_of("POSTCODE").unwrap());
+    let postcode: &str = matches.value_of("POSTCODE").unwrap();
+
+    let location = get_postcode_location(&postcode);
 
 }
 
