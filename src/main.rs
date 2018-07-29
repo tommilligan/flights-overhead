3a20e9aa2befcdf5e50cb7b2428eb91cdbb128fa
Tom Milligan <tommilligan@users.noreply.github.com>
2018-07-29 16:24:49 +0100

    Working clap parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+extern crate clap;
+use clap::{Arg, App};
+
+/// Returns the clap command line parser
+fn main_parser<'a>() -> clap::App<'a, 'a> {
+    App::new("flights-overhead")
+        .arg(Arg::with_name("POSTCODE")
+            .help("Postcode to center search area")
+            .required(true)
+            .index(1))
 }
+
+/// main function
+fn main() {
+    let matches = main_parser().get_matches();
+
+    // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
+    // required we could have used an 'if let' to conditionally get the value)
+    println!("Using postcode: {}", matches.value_of("POSTCODE").unwrap());
+
+}
+
